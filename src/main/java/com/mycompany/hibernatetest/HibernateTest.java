50267618f36f911001f49c108fca0526788479ed
Shriek-356 <khangvskiss@gmail.com>
2025-03-13 22:41:00 +0700

    first_create

---
+++ src/main/java/com/mycompany/hibernatetest/HibernateTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hibernatetest;
+
+/**
+ *
+ * @author Asus
+ */
+public class HibernateTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
